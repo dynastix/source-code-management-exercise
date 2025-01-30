8924b560b5a59b88fde4fffb0c3575ef53b8d3b3
Rica2021 <ricardo.rodriguez252002@gmail.com>
2025-01-30 18:40:46 -0500

    Implemented GCD function

---
+++ students_submissions/gcd_rr764.py
@@ -0,0 +1,25 @@
+def gcd(a: int, b: int) -> int:
+    """
+    Calculate the greatest common divisor (GCD) of two integers a and b
+    using the Euclidean algorithm.
+    """
+    
+    # check for remainder (b) equals '0'
+    if b == 0:
+        return a # return GCD
+    else:
+       return gcd(b, a % b) # recursivelly find the GCD
+
+
+
+# Test cases:
+# GCD(252,105) = 21 
+# GCD(48,18) = 6 
+# GCD(1445,1190) = 85 
+
+a = [252, 48, 1445] 
+b = [105, 18, 1190]
+
+for i in range(3):
+    ans = gcd(a[i], b[i])
+    print(f"GCD({a[i]},{b[i]}) = {ans} ")
