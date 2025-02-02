94e8e894bde794329a606b6a23fdd505e8aac095
mdt322 <mdt26@njit.edu>
2025-02-02 14:48:12 -0500

    Made some changes, seperated the gcd function from edge case testing, made the tests take input

--- students_submissions/gcd_mdt26.py
+++ students_submissions/gcd_mdt26.py
@@ -10,37 +10,42 @@ def gcd(a: int, b: int) -> int:
         print("NOTE: Entered integers are equal.")
         return a
     #This switches the inputs if the denominator is greater
-    if (b > a):
-        gcd(b, a)
     #This tests for inputs that are 0
     if (a == 0 or b == 0):
-        print("ERROR: Neither input can be zeros.")
+        print("ERROR: Neither input can be zero.")
         return
     #This tests for negative inputs
     if (a < 0 or b < 0):
         print("ERROR: Neither input can be negative.")
         return
-    #This tests for prime numbers in the parameters
-    if (isPrime(a) and isPrime(b)):
-        print("NOTE: Both integers are prime. Returning 1.")
-        return 1
-    #This tests if a (the dividend) is prime
-    if (isPrime(a)):
-        print("NOTE: Dividend is prime. Returning 1.")
+    #The following tests if either parameter is prime. If both are prime, algo returns 1
+    a_isPrime = isPrime(a)
+    b_isPrime = isPrime(b)
+    if (a_isPrime and b_isPrime):
+        print("NOTE: As both entered values are prime, the algorithm is returning 1.")
         return 1
 
+    #The following executes the actual gcd seperately from the edge case checks so that
+    #the prime check isn't executed at each recurrence.
+    if (b > a):
+        return gcd_er(b, a)
+    else:
+        return gcd_er(a, b)
+
+#This function does the actual gcd operation.
+def gcd_er(a: int, b: int) -> int:
     r = a%b
     if (r == 0):
         return b
     else:
-        return gcd(b, r)
-#    pass
+        return gcd_er(b,r)
 
-#This function tests whether a number is prime
+#This function tests whether a number is prime. If it is, it prints a message saying so.
 def isPrime(a: int) -> bool:
     if (a == 1):
         return False
     if (a == 2):
+        print(f'{a} is prime.')
         return True
     sqrt_a = a ** 0.5
     i = 2
@@ -48,15 +53,12 @@ def isPrime(a: int) -> bool:
         if (a % i == 0):
             return False
         i += 1
+    print(f'{a} is prime.')
     return True
 
-#TESTS
-#print(gcd(54, 24))  # Expected output: 6
-#print(gcd(24, 54))  # Expected output: 6, same as above
-#print(gcd(48, 18))  # Expected output: 6
-#print(gcd(101, 10))  # Expected output: prints a note, and 1
-#print(gcd(100, 0)) # Expected output: prints error message
-#print(gcd(100, -2)) # Expected output: prints error message
-#print(gcd(100,100)) # Expected output: prints a note indicating equality
-#print(gcd(21, 4)) # Expected output: 1
-#print(gcd(100,5)) # Expected output: 5
+#Testing
+print("a = ", end='')
+a = int(input())
+print('b = ', end='')
+b = int(input())
+print(f'gcd({a}, {b}) = {gcd(a,b)}')
