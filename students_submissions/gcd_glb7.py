556e2b2e612b2558be2a887d748d5c9a95ddf469
GLB7 <124634022+GLB7@users.noreply.github.com>
2025-02-02 20:47:23 -0500

    Implemented GCD function

--- students_submissions/gcd_glb7.py
+++ students_submissions/gcd_glb7.py
@@ -6,9 +6,12 @@
 # Description: Function to calculate the greatest common divisor of two integers using the euclidian method without using any loops aka using recursion.
 
 def gcd(a: int, b: int) -> int:
-    # Edge Case
+    # Edge Cases
     if a == 0 and b == 0:
         print("Undefined value for gcd(0, 0)")
+        return None
+    elif not isinstance(a, int) or not isinstance(b, int):
+        print("Invalid input. Please enter integers only.")
         return None
     # Base Case
     if b == 0:
@@ -26,6 +29,7 @@ print(gcd(-45, -10)) # both negative (a>b)
 print(gcd(5, 0)) # b is 0
 print(gcd(0, 5)) # a is 0
 print(gcd(0, 0))  # Undefined Case (a=0, b=0)
+print(gcd(3.5, "a")) # Invalid input
 print(gcd(598740279859771020868226391647315179701125535902784618637527, 370370240865192802474151282959990087846199968994424134020533)) # large numbers
 
 # Recursion depth case?
